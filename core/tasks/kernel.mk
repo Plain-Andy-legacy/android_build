2119bd1928e3f4242c3f8993f899846588093463
liquid0624 <rjbirnesser@gmail.com>
2015-02-23 10:48:48 -0500

    Custom device toolchain board flags
    Allows devices to use custom tool
    chains for the ROM or the kernel
    
    Examples: TARGET_GCC_VERSION_ARM := arm-eabi-4.9-ln (Kernel)
              TARGET_GCC_VERSION_EXP := 4.8-sm (ROM)

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -151,11 +151,11 @@ ifeq ($(TARGET_ARCH),arm)
         endif
       endif
     endif
-    ifneq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),)
+    ifneq ($(TARGET_GCC_VERSION_ARM),)
         ifeq ($(HOST_OS),darwin)
-            ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/toolchain/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
+            ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/toolchain/$(TARGET_GCC_VERSION_ARM)/bin/arm-eabi-"
         else
-            ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/toolchain/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
+            ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/toolchain/$(TARGET_GCC_VERSION_ARM)/bin/arm-eabi-"
         endif
     else
         ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
